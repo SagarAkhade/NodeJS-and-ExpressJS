f46ca0390bdf33562ed01f201999102777eaf4ef
Sagar Akhade <sagar.akhade123456@gmail.com>
2023-08-22 20:04:44 +0530

    REPL in NodeJS

---
+++ 39_REPL_in_NodeJS/39_REPL_in_NodeJS.js
@@ -0,0 +1,15 @@
+/*
+* REPL in NodeJS ->>>
+    - REPL stands for Read Eval Print Loop  [Eval - Evaluate]
+    - REPL is a command line interface in NodeJS where we can run the NodeJS code or simply JS code
+    - REPL is basically the command line tool
+    - To use the REPL just open the command prompt and type 'node' now it will automatically becomes REPL editor
+    - Now to use it just type the normal code like done below
+        2+5   // output : 7
+        a=3;b=5; a+b  // output : 8
+        like that we can also write the funtions in REPL
+    - In REPL we can switch to Editor mode by using the below command in REPL
+        .editor
+    - After entering to the editor mode we can write the code and to finish the editor mode just 
+      press ctrl + d and to close the editor mode press ctrl + c
+*/
